a8e6766f9d53f021d3cd51399a789d955e12b903
grandeljay <github.jay@grandel.anonaddy.me>
2023-05-11 20:16:02 +0200

    refactor: use src-mmlc directory

--- src-mmlc/Constants.php
+++ src-mmlc/Constants.php
@@ -5,5 +5,5 @@ namespace Grandeljay\Oss;
 class Constants
 {
     public const MODULE_NAME    = 'MODULE_ORDER_TOTAL_GRANDELJAY_OSS';
-    public const MODULE_VERSION = '0.1.0';
+    public const MODULE_VERSION = '0.1.1';
 }
