6647e2b88f5ebba20aca818236b43279edca5af6
grandeljay <github@grandels.email>
2023-11-23 12:47:08 +0100

    chore(module): update to v0.1.2

--- src-mmlc/Constants.php
+++ src-mmlc/Constants.php
@@ -5,5 +5,5 @@ namespace Grandeljay\Oss;
 class Constants
 {
     public const MODULE_NAME    = 'MODULE_ORDER_TOTAL_GRANDELJAY_OSS';
-    public const MODULE_VERSION = '0.1.1';
+    public const MODULE_VERSION = '0.1.2';
 }
